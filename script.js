89ce288a5c7dc02a33e6c5bb344eff7c4fa0daa8
devciente <79010468+FelipeSaimon@users.noreply.github.com>
2022-03-30 12:38:33 -0300

    ajustando

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ function formulario(){
 		if (document.form.email.value == ""){
 			alert("Vazio");
 			document.form.email.focus();
+			document.form.email.value == "";
+			document.form.senha.value == "";
 			return false;
 		}
 
 		if (document.form.email.value == "Web.site@exemplo.com" && document.form.senha.value == 123456){
 			document.location.assign("carregando.html");
 			document.form.email.value == "";
-			document.form.senha.value == ""
+			document.form.senha.value == "";
 			return true;
 		}else
 			alert("Insire um email e uma senha validos");
