23622f7814dbdea885ec0503ed1a846e3f97f7fb
devciente <79010468+FelipeSaimon@users.noreply.github.com>
2022-03-30 12:37:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function formulario(){
 
 		if (document.form.email.value == "Web.site@exemplo.com" && document.form.senha.value == 123456){
 			document.location.assign("carregando.html");
+			document.form.email.value == "";
+			document.form.senha.value == ""
 			return true;
 		}else
 			alert("Insire um email e uma senha validos");
